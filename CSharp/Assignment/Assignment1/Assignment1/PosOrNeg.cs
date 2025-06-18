696c9708e47e3e378aa5de79ada15b63c10f1488
Naveen Thiyagasundaram <naveent@infinite.com>
2025-06-18 20:24:44 +0530

    Adding Csharp assignments

---
+++ CSharp/Assignment/Assignment1/Assignment1/PosOrNeg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Assignment1
+{
+    class PosOrNeg
+    {
+        public static void posorneg()
+        {
+            Console.Write("Enter a Number : "); 
+            int Input = int.Parse(Console.ReadLine()); //Getting input from the user
+            string result = (Input > 0) ? $"Entered Number {Input} is Positive" : $"Entered Number {Input} is Negative";//checking Positive or Negative
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp/Assignment/Assignment1/Assignment1/TripleOfIntegers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Assignment1
+{
+    class TripleOfIntegers
+    {
+        public static void tripleofintegers()
+        {
+            Console.Write("First Integer : ");                                            //getting First Input
+            int input1 = int.Parse(Console.ReadLine()); 
+
+            Console.Write("Second Integer : ");                                           //getting second Input
+            int input2 = int.Parse(Console.ReadLine());
+
+            if (input1 != input2)  Console.WriteLine($"The sum is : {input1+input2}");    //If Not equal sum
+            else Console.WriteLine($"The triplet of Integer is : {input1*3}");            //If equal triple of sum
+        }
+    }
+}
