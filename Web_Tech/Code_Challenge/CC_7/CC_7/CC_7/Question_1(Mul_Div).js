e68d185d8e800bc95ed2107939ed8b1539abf069
Navee090603 <tk.naveen.tk@gmail.com>
2025-08-06 11:32:33 +0530

    cc7

---
+++ Web_Tech/Code_Challenge/CC_7/CC_7/CC_7/Question_1(Mul_Div).js
@@ -0,0 +1,17 @@
+﻿function multiply() {
+    var num1 = document.getElementById("firstNumber").value;
+    var num2 = document.getElementById("secondNumber").value;
+    var result = Number(num1) * Number(num2);
+    document.getElementById("result").innerHTML = "The Result Is : " + result;
+}
+
+function divide() {
+    var num1 = document.getElementById("firstNumber").value;
+    var num2 = document.getElementById("secondNumber").value;
+    if (Number(num2) === 0) {
+        document.getElementById("result").innerHTML = "The Result Is : Cannot divide by zero";
+    } else {
+        var result = Number(num1) / Number(num2);
+        document.getElementById("result").innerHTML = "The Result Is : " + result;
+    }
+}
---
+++ Web_Tech/Code_Challenge/CC_7/CC_7/CC_7/Question_2(Odd_Even).js
@@ -0,0 +1,11 @@
+﻿function checkOddEven() {
+    var output = "";
+    for (var i = 0; i <= 15; i++) {
+        if (i % 2 === 0) {
+            output += i + " is even<br>";
+        } else {
+            output += i + " is odd<br>";
+        }
+    }
+    document.getElementById("oddevenresult").innerHTML = output;
+}
